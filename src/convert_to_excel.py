c6a8017a6fcc55ed4e9b7caaa0535d277d47a208
huythanh0x <huythanh0x@gmail.com>
2022-06-24 07:09:44 +0700

    fix wb utf8

--- src/convert_to_excel.py
+++ src/convert_to_excel.py
@@ -1,17 +1,13 @@
 import os
 import glob
-import csv
-from openpyxl import Workbook
 import pandas as pd
 def convert_to_excel(file_name):
-    wb = Workbook()
-    ws = wb.active
-    with open(file_name, 'r') as f:
-        for row in csv.reader(f):
-            ws.append(row)
     pure_file_name = file_name[16:-4]
     print(pure_file_name)
-    wb.save(f'data_excel/{pure_file_name}.xlsx')
+    new_csv_file_path = f"output_data_csv/{pure_file_name}.csv"
+    data = pd.read_csv(new_csv_file_path)
+    output_excel_name = f'data_excel/{pure_file_name}.xlsx'
+    data.to_excel(output_excel_name, index=None, header=True)
 
 def remove_all_excel_files():
     for csvfile in glob.glob(os.path.join('data_excel', '*.xlsx')):
@@ -30,8 +26,8 @@ def rank_student_by_total_score():
         df['Tổng'] = df['Tổng'].astype(float) 
         df = df.sort_values(by=['Tổng'], ascending=False)
         df.insert(10, 'Xếp hạng', range(1,len(df)+1))
-        # print(df.to_string()) 
-        df.to_csv(new_csv_file_path, sep=',',index=False)
+        df.to_csv(new_csv_file_path, sep=',',index=False,encoding="utf8")
+
 rank_student_by_total_score()
 remove_all_excel_files()
 convert_all_to_excel_files()
